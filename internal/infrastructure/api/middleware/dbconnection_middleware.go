131478968180daec7ff225919209fc58c72b4a3e
Marlon H. Garcia <marlon.hg2003@gmail.com>
2025-03-31 02:39:16 -0600

    fix: correccion de timeout de conexion en las DB externas

---
+++ internal/infrastructure/api/middleware/dbconnection_middleware.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"github.com/MarlonG1/api-facturacion-sv/config/drivers"
+	"github.com/MarlonG1/api-facturacion-sv/pkg/shared/logs"
+	"net/http"
+)
+
+type DBConnectionMiddleware struct {
+	connection *drivers.DbConnection
+}
+
+func NewDBConnectionMiddleware(connection *drivers.DbConnection) *DBConnectionMiddleware {
+	return &DBConnectionMiddleware{connection: connection}
+}
+
+func (m *DBConnectionMiddleware) Handler(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sqlDB, err := m.connection.Db.DB()
+
+		if err != nil {
+			logs.Error("Error connecting to database", map[string]interface{}{
+				"error": err.Error(),
+			})
+			http.Error(w, "Error connecting to database", http.StatusInternalServerError)
+			return
+		}
+		sqlDB.Ping()
+
+		logs.Debug("Database connection established")
+		next.ServeHTTP(w, r)
+	})
+}
